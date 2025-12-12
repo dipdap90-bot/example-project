1c7849851cb725d4aa0a7c086dd8884c8f255645
bilal7355 <bilalahmed7355@gmail.com>
2025-12-12 11:50:11 +0530

    final commit

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 #app.py
+from flask import Flask,render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+    api_key = os.getenv('FLIGHT_API')
+    return render_template('predict.html', result=api_key)
+
+if __name__ == '__main__':
+    app.run(debug=True)
